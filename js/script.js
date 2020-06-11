1f864781fe360dc3b99117e48f6cd9f833dad9a3
lindam <linda.minotti2@gmail.com>
2020-06-11 16:26:02 +0200

    Sistemato dopo correzione in classe.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,32 @@
 // Loscript di JS viene caricato solo dopo aver letto tutto l'html
 $(document).ready(
   function() {
-
+    // Mouse enter fa vedere il menu dropdown
     $('.with-dropdown').mouseenter(
       function() {
         $(this).children('.dropdown').addClass('open');
       }
     )
+    // Mouse leave nasconde il menu dropdown
     $('.with-dropdown').mouseleave(
       function() {
         $(this).children('.dropdown').removeClass('open');
       }
     )
+    // Al click rende visibile o nasconde il menu dropdown
     $('.with-dropdown').click(
       function() {
-        $(this).children('.dropdown').removeClass('open');
+        $(this).children('.dropdown').toggleClass('open');
       }
     )
+    // Mouse enter gira la freccia it
     $('.it-arrow').mouseenter(
       function() {
         $(this).find('.fa').removeClass('fa-chevron-up');
         $(this).find('.fa').addClass('fa-chevron-down');
       }
     )
+    // Mouse leave gira la freccia it
     $('.it-arrow').mouseleave(
       function() {
         $(this).find('.fa').removeClass('fa-chevron-down');
